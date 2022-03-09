f269346b96ea213b5b537e75797b7d9d0a03aeb5
Longinus <jesuscaesar@outlook.com>
2022-03-09 20:18:58 +1000

    Latest update

---
+++ getman.php
@@ -0,0 +1,33 @@
+<?php if ($_REQUEST['seq'] == 'yes') { ?>
+<input id="enterSeq" type="text" style="width:72%;" placeholder="List the GET command sequences" value="" onkeydown="if (event.keyCode == 13) {
+    seq(enterSeq.value);
+}">
+<input type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="seq(enterSeq.value);" value=">">
+<input type="button" class="actionButtonRed" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="window.location.href='?seq=no';" value="!">
+<?php } else { ?>
+<select id="enterKey" onchange="
+var curSys = getButton.name;
+var keyVal = enterKey.options[enterKey.selectedIndex].value;
+if (keyVal == 'i') {
+    enterPkg.value = 'from';
+    enterRepo.value = '';
+    enterUser.value = '';
+} else if (keyVal == 'r') {
+    enterPkg.value = curSys;
+    enterRepo.value = '';
+    enterUser.value = '';
+} else if (keyVal == 'd') {
+    enterPkg.value = '';
+    enterRepo.value = 'from';
+    enterUser.value = 'here';
+}">
+<option value='i'>Install</option>
+<option value='r'>Replace</option>
+<option value='d'>Remove</option>
+</select>
+<input type="text" id="enterPkg" style="width:20%;" placeholder="Package" value="from">
+<input type="text" id="enterRepo" style="width:20%;" placeholder="Repo" value="">
+<input type="text" id="enterUser" style="width:20%;" placeholder="User" value="">
+<input id='getButton' name="<?=file_get_contents('system.info');?>" type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="get(enterKey.options[enterKey.selectedIndex].value,'',enterPkg.value,enterRepo.value,'',enterUser.value);" value=">">
+<input type="button" class="actionButtonRed" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="window.location.href='?seq=yes';" value="!">
+<?php } ?>
--- packages.php
+++ packages.php
@@ -21,40 +21,7 @@ window.onload = function() {
 <body>
 <div class='top'>
 <p align="center">
-<?php if ($_REQUEST['seq'] == 'yes') { ?>
-<input id="enterSeq" type="text" style="width:72%;" placeholder="List the GET command sequences" value="" onkeydown="if (event.keyCode == 13) {
-    seq(enterSeq.value);
-    window.location.href = 'index.php';
-}">
-<input type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="seq(enterSeq.value); window.location.href = 'index.php';" value=">">
-<input type="button" class="actionButtonRed" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="window.location.href='?seq=no';" value="!">
-<?php } else { ?>
-<select id="enterKey" onchange="
-var curSys = getButton.name;
-var keyVal = enterKey.options[enterKey.selectedIndex].value;
-if (keyVal == 'i') {
-    enterPkg.value = 'from';
-    enterRepo.value = '';
-    enterUser.value = '';
-} else if (keyVal == 'r') {
-    enterPkg.value = curSys;
-    enterRepo.value = '';
-    enterUser.value = '';
-} else if (keyVal == 'd') {
-    enterPkg.value = '';
-    enterRepo.value = 'from';
-    enterUser.value = 'here';
-}">
-<option value='i'>Install</option>
-<option value='r'>Replace</option>
-<option value='d'>Remove</option>
-</select>
-<input type="text" id="enterPkg" style="width:20%;" placeholder="Package" value="from">
-<input type="text" id="enterRepo" style="width:20%;" placeholder="Repo" value="">
-<input type="text" id="enterUser" style="width:20%;" placeholder="User" value="">
-<input id='getButton' onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" name="<?=file_get_contents('system.info');?>" type="button" class="actionButtonGreen" onclick="get(enterKey.options[enterKey.selectedIndex].value, '', enterPkg.value, enterRepo.value, '', enterUser.value); window.location.href = 'index.php';" value=">">
-<input type="button" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" class="actionButtonRed" onclick="window.location.href='?seq=yes';" value="!">
-<?php } ?>
+<?php include 'getman.php'; ?>
 </p>
 </div>
 <div class='panel'>
