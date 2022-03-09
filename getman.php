33ac83f1af8000e82e973a65a0923168d417d066
Longinus <jesuscaesar@outlook.com>
2022-03-09 20:21:38 +1000

    Latest update

--- getman.php
+++ getman.php
@@ -1,8 +1,9 @@
 <?php if ($_REQUEST['seq'] == 'yes') { ?>
 <input id="enterSeq" type="text" style="width:72%;" placeholder="List the GET command sequences" value="" onkeydown="if (event.keyCode == 13) {
     seq(enterSeq.value);
+    window.location.href = 'index.php';
 }">
-<input type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="seq(enterSeq.value);" value=">">
+<input type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="seq(enterSeq.value); window.location.href = 'index.php';" value=">">
 <input type="button" class="actionButtonRed" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="window.location.href='?seq=no';" value="!">
 <?php } else { ?>
 <select id="enterKey" onchange="
@@ -28,6 +29,6 @@ if (keyVal == 'i') {
 <input type="text" id="enterPkg" style="width:20%;" placeholder="Package" value="from">
 <input type="text" id="enterRepo" style="width:20%;" placeholder="Repo" value="">
 <input type="text" id="enterUser" style="width:20%;" placeholder="User" value="">
-<input id='getButton' name="<?=file_get_contents('system.info');?>" type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="get(enterKey.options[enterKey.selectedIndex].value,'',enterPkg.value,enterRepo.value,'',enterUser.value);" value=">">
+<input id='getButton' name="<?=file_get_contents('system.info');?>" type="button" class="actionButtonGreen" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="get(enterKey.options[enterKey.selectedIndex].value,'',enterPkg.value,enterRepo.value,'',enterUser.value); window.location.href = 'index.php';" value=">">
 <input type="button" class="actionButtonRed" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" onclick="window.location.href='?seq=yes';" value="!">
 <?php } ?>
